015190f4840fbc9b4e68fceea25628e4d1456ce3
evan <evanmcfarland.aa@gmail.com>
2023-10-03 13:02:22 -0400

    Bookcard image low-res. Virtualbookshelf styled.

--- src/ugd_frontend/src/cards/BookCard.tsx
+++ src/ugd_frontend/src/cards/BookCard.tsx
@@ -1,15 +1,16 @@
-// // OG While flipped-state is here:
-// import React, { useState, useEffect, useRef } from 'react';
+// import React, { useEffect, useRef } from 'react';
 // import '../../styles/BookCard.css';
 
 // interface CardProps {
 //   image: string;
 //   title: string;
 //   description: string;
+//   flipped: boolean;
+//   onCardClick: () => void;
+//   onReadBookClick: (event: React.MouseEvent) => void;
 // }
 
-// const BCBookCard: React.FC<CardProps> = ({ image, title, description }) => {
-//   const [flipped, setFlipped] = useState(false);
+// const BCBookCard: React.FC<CardProps> = ({ image, title, description, flipped, onCardClick, onReadBookClick }) => {
 //   const titleRef = useRef<HTMLParagraphElement>(null);
 
 //   useEffect(() => {
@@ -21,28 +22,24 @@
 //     }
 //   }, []);
 
-//   const onCardClick = () => {
-//     setFlipped(!flipped);
-//   };
+//   let dynamicFontSize = '1.3rem';
+//   let dynamicLineHeight = 0.8;
+//   let displayTitle = title;
+//   const titleLength = title.length;
 
-// let dynamicFontSize = '1.3rem';
-// let dynamicLineHeight = 0.8;
-// let displayTitle = title;
-// const titleLength = title.length;
+//   const maxTitleLength = 65; 
 
-// const maxTitleLength = 65; 
+//   for (let i = 0; i < titleLength; i++) {
+//     if (i % 5 === 0 && i !== 0) {
+//       dynamicFontSize = `${parseFloat(dynamicFontSize) - 0.04}rem`;
+//       dynamicLineHeight += 0.03;
+//     }
 
-// for (let i = 0; i < titleLength; i++) {
-//   if (i % 5 === 0 && i !== 0) {
-//     dynamicFontSize = `${parseFloat(dynamicFontSize) - 0.04}rem`;
-//     dynamicLineHeight += 0.03;
+//     if (i >= maxTitleLength) {
+//       displayTitle = `${title.substring(0, i)}...`;
+//       break;
+//     }
 //   }
-
-//   if (i >= maxTitleLength) {
-//     displayTitle = `${title.substring(0, i)}...`;
-//     break;
-//   }
-// }
 
 
 //   return (
@@ -67,6 +64,12 @@
 //         </div>
 //         <div className="BCCard-face BCCard-back absolute inset-0 bg-[#fbfbf8] text-center p-10 overflow-y-auto">
 //           <p className="BCStreamed-description">{description}</p>
+//           <button 
+//             className="BCReadBook-button" 
+//             onClick={onReadBookClick}
+//           >
+//             Read Book
+//           </button>
 //         </div>
 //       </div>
 //     </div>
@@ -84,9 +87,20 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
 import React, { useState, useEffect, useRef } from 'react';
 import '../../styles/BookCard.css';
-import { handleReadBookClick } from '../../utils/handleReadBookClick';
+import Resizer from 'react-image-file-resizer'
 
 interface CardProps {
   image: string;
@@ -128,6 +142,30 @@ const BCBookCard: React.FC<CardProps> = ({ image, title, description, flipped, o
     }
   }
 
+  const [compressedImageSrc, setCompressedImageSrc] = useState<string>("");
+
+  useEffect(() => {
+    if (image) {
+      fetch(image)
+        .then(response => response.blob())
+        .then(blob => {
+          Resizer.imageFileResizer(
+            blob,
+            300,
+            300,
+            'PNG',
+            90,
+            0,
+            (uri) => {
+              if (typeof uri === 'string') {
+                setCompressedImageSrc(uri);
+              }
+            },
+            'base64'
+          );
+        });
+    }
+  }, [image]);  
 
   return (
     <div
@@ -137,7 +175,7 @@ const BCBookCard: React.FC<CardProps> = ({ image, title, description, flipped, o
       <div className="BCBook-card relative transform-gpu">
         <div className="BCCard-face BCCard-front absolute inset-0 bg-[#fbfbf8] border border-[#252525] rounded-[.5rem] flex flex-col items-center">
           <div className="BCImage-container w-full h-full minus-mb-[25px] overflow-hidden rounded-t-[.5rem] shadow-inner">
-            <img src={image} className="object-cover w-full h-full" alt={title} />
+            <img src={compressedImageSrc} className="object-cover w-full h-full" alt={title} />
           </div>
           <div className="BCText-container w-[95%] h-[30px] rounded-b-lg flex items-center justify-center">
             <p
