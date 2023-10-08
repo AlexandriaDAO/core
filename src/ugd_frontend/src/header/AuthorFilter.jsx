d3f4243fa7e766ccd0f419eb5525da7d36c561eb
evan <evanmcfarland.aa@gmail.com>
2023-10-08 10:43:54 -0400

    Category filters for the grid.

--- src/ugd_frontend/src/header/AuthorFilter.jsx
+++ src/ugd_frontend/src/header/AuthorFilter.jsx
@@ -1,9 +1,67 @@
+// import React from 'react';
+// import AUTHOR_INFO from '../../assets/author_data';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faTimes, faBars } from '@fortawesome/free-solid-svg-icons';
+
+// const AuthorFilter = ({ isDropdownVisible, toggleDropdown, selectedAuthors, handleAuthorSelection, handleAllBooksSelection }) => (
+//   <>
+//     <button className="filter-icon-button" onClick={toggleDropdown}>
+//       <FontAwesomeIcon icon={isDropdownVisible ? faTimes : faBars} />
+//     </button>
+//     {isDropdownVisible && (
+//       <div className="filter-popup">
+//         <div className="filter-item">
+//           <input 
+//             type="checkbox" 
+//             id="all-books" 
+//             checked={selectedAuthors.length === AUTHOR_INFO.length}
+//             onChange={handleAllBooksSelection}
+//           />
+//           <label htmlFor="all-books">All Books</label>
+//         </div>
+//         {AUTHOR_INFO.map(author => (
+//           <div key={author.id} className="filter-item">
+//             <input 
+//               type="checkbox"
+//               id={author.id}
+//               checked={selectedAuthors.includes(author.id)}
+//               onChange={() => handleAuthorSelection(author.id)}
+//             />
+//             <label htmlFor={author.id}>{author.id}</label>
+//           </div>
+//         ))}
+//       </div>
+//     )}
+//   </>
+// );
+
+// export default AuthorFilter;
+
+
+
+
+
+
+
+
+
+
+
 import React from 'react';
 import AUTHOR_INFO from '../../assets/author_data';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes, faBars } from '@fortawesome/free-solid-svg-icons';
 
-const AuthorFilter = ({ isDropdownVisible, toggleDropdown, selectedAuthors, handleAuthorSelection, handleAllBooksSelection }) => (
+const AuthorFilter = ({ 
+    isDropdownVisible, 
+    toggleDropdown, 
+    selectedAuthors, 
+    handleAuthorSelection, 
+    handleAllBooksSelection,
+    allCategories,
+    selectedCategories,
+    handleCategorySelection 
+}) => (
   <>
     <button className="filter-icon-button" onClick={toggleDropdown}>
       <FontAwesomeIcon icon={isDropdownVisible ? faTimes : faBars} />
@@ -19,7 +77,23 @@ const AuthorFilter = ({ isDropdownVisible, toggleDropdown, selectedAuthors, hand
           />
           <label htmlFor="all-books">All Books</label>
         </div>
-        {AUTHOR_INFO.map(author => (
+
+        {allCategories.map(category => (
+          <div key={category} className="filter-item">
+            <input 
+              type="checkbox"
+              id={category}
+              checked={selectedCategories.includes(category)}
+              onChange={() => handleCategorySelection(category)}
+            />
+            <label htmlFor={category}>{category}</label>
+          </div>
+        ))}
+
+        {AUTHOR_INFO.filter(author => 
+          selectedCategories.some(cat => author.category.includes(cat)) ||
+          selectedCategories.length === 0
+        ).map(author => (
           <div key={author.id} className="filter-item">
             <input 
               type="checkbox"
--- src/ugd_frontend/src/index.js
+++ src/ugd_frontend/src/index.js
@@ -21,6 +21,7 @@ const App = () => {
   const [imageUrl, setImageUrl] = useState(null);
   const backgroundPosition = useBackgroundPosition();
   const [selectedAuthors, setSelectedAuthors] = useState(AUTHOR_INFO.map(author => author.id));
+  const [selectedCategories, setSelectedCategories] = useState([]);
 
   useEffect(() => {
     const image = require.context('../assets/public/images/', false, /\.(png|jpe?g|svg)$/);
@@ -46,11 +47,19 @@ const App = () => {
       )}
       <div style={{ paddingTop: '25px' }}>
         <Tabs/>
-        {/* <SearchBar/>  */}
-        <SearchBar selectedAuthors={selectedAuthors} setSelectedAuthors={setSelectedAuthors} />
-        <div className='main-grid-container'>
-          <AuthorPanel authors={AUTHOR_INFO.filter(author => selectedAuthors.includes(author.id))} />
-        </div>
+      <SearchBar 
+        selectedAuthors={selectedAuthors} 
+        setSelectedAuthors={setSelectedAuthors} 
+        selectedCategories={selectedCategories} 
+        setSelectedCategories={setSelectedCategories}
+      />
+      <div className='main-grid-container'>
+        <AuthorPanel authors={AUTHOR_INFO.filter(author => 
+          selectedAuthors.includes(author.id) &&
+          (selectedCategories.length === 0 || 
+           selectedCategories.some(cat => author.category.includes(cat)))
+        )} />
+      </div>
       </div>
     </div>
   );
@@ -83,118 +92,3 @@ const App = () => {
     });
   }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Compbined Version: 
-// // index.js
-// import React, { useEffect, useState } from 'react';
-// import { createRoot } from 'react-dom/client';
-// import SemanticLibrary from './semantic-library/SemanticLibrary';
-// import { ugd_backend } from "../../declarations/ugd_backend";
-// import useBackgroundPosition from '../utils/useBackgroundPosition';
-// import '../styles/main.css';
-// import AUTHOR_INFO from '../assets/author_data';
-// import AuthorCards from './the-greats/AuthorCards'
-
-// // Your original HomePage code, now embedded into this file
-// function HomePage({ authors }) {
-//   const [activeAuthor, setActiveAuthor] = useState(null);
-
-//   useEffect(() => {
-//     setActiveAuthor(null);
-//   }, []);
-
-//   const handleCardClick = (authorId) => {
-//     setActiveAuthor(authorId === activeAuthor ? null : authorId);
-//   };
-
-//   return (
-//     <div className="grid">
-//       {authors.map((author) => (
-//         <div key={author.id} onClick={() => handleCardClick(author.id)}>
-//           {/* Assume AuthorCard is available or imported */}
-//           <AuthorCards 
-//             author={author} 
-//             expanded={activeAuthor === author.id} 
-//           />
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// const App = () => {
-//   const [imageUrl, setImageUrl] = useState(null);
-//   const backgroundPosition = useBackgroundPosition();
-
-//   useEffect(() => {
-//     const image = require.context('../assets/public/images/', false, /\.(png|jpe?g|svg)$/);
-//     setImageUrl(image('./BlackedOut.png').default);
-//   }, []);
-
-//   return (
-//     <div style={{ position: 'relative', minHeight: '100vh' }}>
-//       {imageUrl && (
-//         <div id="imageContainer" style={{
-//           backgroundImage: `url(${imageUrl})`,
-//           backgroundPosition: backgroundPosition,
-//           backgroundSize: 'cover',
-//           backgroundAttachment: 'fixed',
-//           position: 'absolute',
-//           top: 0,
-//           left: 0,
-//           width: '100%',
-//           height: '100%',
-//           opacity: '0.1',
-//           zIndex: -1,
-//         }} />
-//       )}
-//       <div>
-//         <SemanticLibrary />
-//       </div>
-//       {/* Embed HomePage right under SemanticLibrary */}
-//       <div>
-//         {/* <HomePage authors={AUTHOR_INFO} /> */}
-//       </div>
-//     </div>
-//   );
-// };
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   const semanticLibraryRoot = document.getElementById("semantic-library-root");
-//   if (semanticLibraryRoot) {
-//     const root = createRoot(semanticLibraryRoot);
-//     root.render(
-//       <React.StrictMode>
-//         <App />
-//       </React.StrictMode>
-//     );
-//   }
-
-//   const form = document.querySelector("form");
-//   if (form) {
-//     form.addEventListener("submit", async (e) => {
-//       e.preventDefault();
-//       const button = e.target.querySelector("button");
-//       const name = document.getElementById("name").value.toString();
-//       button.setAttribute("disabled", true);
-
-//       const greeting = await ugd_backend.greet(name);
-//       button.removeAttribute("disabled");
-//       document.getElementById("greeting").innerText = greeting;
-
-//       return false;
-//     });
-//   }
-// });
