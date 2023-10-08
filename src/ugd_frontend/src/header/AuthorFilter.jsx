9bb2a24faa001f5ceb92a258412702a075478eb8
evan <evanmcfarland.aa@gmail.com>
2023-10-08 14:03:51 -0400

    Search Filter element structured/basic styles.

--- src/ugd_frontend/src/header/AuthorFilter.jsx
+++ src/ugd_frontend/src/header/AuthorFilter.jsx
@@ -1,56 +1,22 @@
-// import React from 'react';
-// import AUTHOR_INFO from '../../assets/author_data';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faTimes, faBars } from '@fortawesome/free-solid-svg-icons';
-
-// const AuthorFilter = ({ isDropdownVisible, toggleDropdown, selectedAuthors, handleAuthorSelection, handleAllBooksSelection }) => (
-//   <>
-//     <button className="filter-icon-button" onClick={toggleDropdown}>
-//       <FontAwesomeIcon icon={isDropdownVisible ? faTimes : faBars} />
-//     </button>
-//     {isDropdownVisible && (
-//       <div className="filter-popup">
-//         <div className="filter-item">
-//           <input 
-//             type="checkbox" 
-//             id="all-books" 
-//             checked={selectedAuthors.length === AUTHOR_INFO.length}
-//             onChange={handleAllBooksSelection}
-//           />
-//           <label htmlFor="all-books">All Books</label>
-//         </div>
-//         {AUTHOR_INFO.map(author => (
-//           <div key={author.id} className="filter-item">
-//             <input 
-//               type="checkbox"
-//               id={author.id}
-//               checked={selectedAuthors.includes(author.id)}
-//               onChange={() => handleAuthorSelection(author.id)}
-//             />
-//             <label htmlFor={author.id}>{author.id}</label>
-//           </div>
-//         ))}
-//       </div>
-//     )}
-//   </>
-// );
-
-// export default AuthorFilter;
-
-
-
-
-
-
-
-
-
-
-
-import React from 'react';
+import React, { useState } from 'react';
 import AUTHOR_INFO from '../../assets/author_data';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faBars, faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import '../../styles/AuthorFilter.css'
+
+const CollapsibleSection = ({ title, children }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div className="collapsible-section">
+            <button className="collapsible-header" onClick={() => setIsOpen(!isOpen)}>
+                {title}
+                <FontAwesomeIcon icon={isOpen ? faChevronUp : faChevronDown} />
+            </button>
+            {isOpen && <div className="collapsible-content">{children}</div>}
+        </div>
+    );
+}
 
 const AuthorFilter = ({ 
     isDropdownVisible, 
@@ -62,51 +28,56 @@ const AuthorFilter = ({
     selectedCategories,
     handleCategorySelection 
 }) => (
-  <>
-    <button className="filter-icon-button" onClick={toggleDropdown}>
-      <FontAwesomeIcon icon={isDropdownVisible ? faTimes : faBars} />
-    </button>
-    {isDropdownVisible && (
-      <div className="filter-popup">
-        <div className="filter-item">
-          <input 
-            type="checkbox" 
-            id="all-books" 
-            checked={selectedAuthors.length === AUTHOR_INFO.length}
-            onChange={handleAllBooksSelection}
-          />
-          <label htmlFor="all-books">All Books</label>
-        </div>
+    <>
+        <button className="filter-icon-button" onClick={toggleDropdown}>
+            <FontAwesomeIcon icon={isDropdownVisible ? faTimes : faBars} />
+        </button>
+        {isDropdownVisible && (
+            <div className="filter-popup">
+                <div className="filter-item">
+                    <input 
+                        type="checkbox" 
+                        id="all-books" 
+                        checked={selectedAuthors.length === AUTHOR_INFO.length}
+                        onChange={handleAllBooksSelection}
+                    />
+                    <label htmlFor="all-books">All Books</label>
+                </div>
 
-        {allCategories.map(category => (
-          <div key={category} className="filter-item">
-            <input 
-              type="checkbox"
-              id={category}
-              checked={selectedCategories.includes(category)}
-              onChange={() => handleCategorySelection(category)}
-            />
-            <label htmlFor={category}>{category}</label>
-          </div>
-        ))}
+                <CollapsibleSection title="Categories">
+                    {allCategories.map(category => (
+                        <div key={category} className="filter-item">
+                            <input 
+                                type="checkbox"
+                                id={category}
+                                checked={selectedCategories.includes(category)}
+                                onChange={() => handleCategorySelection(category)}
+                            />
+                            <label htmlFor={category}>{category}</label>
+                        </div>
+                    ))}
+                </CollapsibleSection>
 
-        {AUTHOR_INFO.filter(author => 
-          selectedCategories.some(cat => author.category.includes(cat)) ||
-          selectedCategories.length === 0
-        ).map(author => (
-          <div key={author.id} className="filter-item">
-            <input 
-              type="checkbox"
-              id={author.id}
-              checked={selectedAuthors.includes(author.id)}
-              onChange={() => handleAuthorSelection(author.id)}
-            />
-            <label htmlFor={author.id}>{author.id}</label>
-          </div>
-        ))}
-      </div>
-    )}
-  </>
+                <CollapsibleSection title="Authors">
+                    {AUTHOR_INFO.filter(author => 
+                        selectedCategories.some(cat => author.category.includes(cat)) ||
+                        selectedCategories.length === 0
+                    ).map(author => (
+                        <div key={author.id} className="filter-item">
+                            <input 
+                                type="checkbox"
+                                id={author.id}
+                                checked={selectedAuthors.includes(author.id)}
+                                onChange={() => handleAuthorSelection(author.id)}
+                            />
+                            <label htmlFor={author.id}>{author.id}</label>
+                        </div>
+                    ))}
+                </CollapsibleSection>
+
+            </div>
+        )}
+    </>
 );
 
 export default AuthorFilter;
