9a2c7c023e34225c174e86eeb2a35dfedfd9e6f4
Zeeshan Tariq <zeeshan.ghazi@yahoo.com>
2023-10-25 01:32:18 -0700

    remove gitignore files

--- src/declarations/ugd_backend/ugd_backend.did.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Principal } from '@dfinity/principal';
-import type { ActorMethod } from '@dfinity/agent';
-
-export interface MessageCard { 'user_query' : string, 'message' : string }
-export interface _SERVICE {
-  'mc_front' : ActorMethod<[string], [] | [MessageCard]>,
-}
