cf29b36fb8e9899032f155400879af4a2ca5a19f
evan <evanmcfarland.aa@gmail.com>
2023-12-02 12:06:02 -0500

    Backend + Frontend Linked with tests. More attributes in sourceCard backend.

--- src/ugd_backend/src/weaviate.rs
+++ src/ugd_backend/src/weaviate.rs
@@ -1,4 +1,96 @@
-use crate::save_source_card;
+// use crate::save_sc;
+
+// use ic_cdk::api::management_canister::http_request::{http_request, CanisterHttpRequestArgument, HttpHeader, HttpMethod};
+// use serde::{Serialize, Deserialize};
+
+
+// #[derive(Serialize, Deserialize)]
+// struct BookSearchRequest {
+//   query: String,
+//   breadth: u8,
+//   scope: String,
+// }
+
+// #[derive(Serialize, Deserialize, Debug)]
+// struct BookSearchResponse {
+//   content: String,
+//   heading: String,
+//   title: String,
+// }
+
+// #[ic_cdk::update]
+// pub async fn get_weaviate_query(user_query: String, breadth: u8, scope: String) -> String {
+
+//   let query_for_request = user_query.clone();
+
+//   let url = "https://www.uncensoredgreats.com/api/IC/bookSearch".to_string();
+
+//   let body = BookSearchRequest {
+//       query: query_for_request,
+//       breadth,
+//       scope,
+//   };
+//   let json_body = serde_json::to_string(&body).unwrap();
+
+//   let request_headers = vec![
+//       HttpHeader {
+//           name: "Content-Type".to_string(),
+//           value: "application/json".to_string(),
+//       },
+//   ];
+
+//   let request = CanisterHttpRequestArgument {
+//       url,
+//       method: HttpMethod::POST,
+//       body: Some(json_body.into_bytes()),
+//       max_response_bytes: None,
+//       transform: None,
+//       headers: request_headers,
+//   };
+
+//   match http_request(request).await {
+//     Ok((response,)) => {
+//         let str_body = String::from_utf8(response.body)
+//             .expect("Response is not UTF-8 encoded.");
+
+//         let parsed_response: Vec<BookSearchResponse> = serde_json::from_str(&str_body)
+//             .expect("Failed to parse JSON");
+
+//         for item in parsed_response {
+//             // ToDo: Updated weaviate cluster, heading should be CFI link, and string by default.
+//             let heading_str = item.heading.to_string();
+
+//             // This is the only link to the rest of the app.
+//             save_sc(user_query.clone(), item.title, heading_str, item.content);
+//         }
+
+//         "Source cards populated successfully".to_string()
+//     }
+//     Err((r, m)) => {
+//         format!("HTTP request error. Code: {r:?}, Message: {m}")
+//     }
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+use crate::save_sc;
 
 use ic_cdk::api::management_canister::http_request::{http_request, CanisterHttpRequestArgument, HttpHeader, HttpMethod};
 use serde::{Serialize, Deserialize};
@@ -21,6 +113,7 @@ struct BookSearchResponse {
 #[ic_cdk::update]
 pub async fn get_weaviate_query(user_query: String, breadth: u8, scope: String) -> String {
 
+  let author_str = scope.clone();
   let query_for_request = user_query.clone();
 
   let url = "https://www.uncensoredgreats.com/api/IC/bookSearch".to_string();
@@ -60,8 +153,8 @@ pub async fn get_weaviate_query(user_query: String, breadth: u8, scope: String) 
             // ToDo: Updated weaviate cluster, heading should be CFI link, and string by default.
             let heading_str = item.heading.to_string();
 
-            // This is the only link to the rest of the app.
-            save_source_card(user_query.clone(), item.title, heading_str, item.content);
+            // This is the only link to the rest of thle app.
+            save_sc(user_query.clone(), author_str.clone(), item.title, heading_str, item.content);
         }
 
         "Source cards populated successfully".to_string()
@@ -71,4 +164,3 @@ pub async fn get_weaviate_query(user_query: String, breadth: u8, scope: String) 
     }
   }
 }
-
--- src/ugd_frontend/src/utils/MessageProvider.tsx
+++ src/ugd_frontend/src/utils/MessageProvider.tsx
@@ -1,3 +1,5 @@
+// OG
+
 import React, { useState } from 'react';
 import { ugd_backend } from '../declarations/ugd_backend';
 import MessageContext from '../contexts/MessageContext';
@@ -11,6 +13,16 @@ interface MessageCard {
   message: string;
 }
 
+interface SourceCard {
+  post_id: bigint;
+  user_query: string;
+  author: string;
+  title: string;
+  heading: string;
+  content: string;
+  bookmarked: boolean;
+};
+
 const MessageProvider: React.FC<MessageProviderProps> = ({ children }) => {
   const [message, setMessage] = useState<MessageCard | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -18,10 +30,10 @@ const MessageProvider: React.FC<MessageProviderProps> = ({ children }) => {
   const [currentAuthorId, setRandomAuthorId] = useState<string | null>(null);
 
   
-  const updateMessage = async (query: string) => {
+  const updateMessage = async (user_query: string) => {
     setIsLoading(true);
     try {
-        const response: MessageCard[] = await ugd_backend.mc_front(query);
+        const response: MessageCard[] = await ugd_backend.mc_front(user_query);
         
         if (response && response.length > 0) {
           const firstResponse = response[0];
@@ -30,6 +42,7 @@ const MessageProvider: React.FC<MessageProviderProps> = ({ children }) => {
               message: firstResponse?.message ?? "",
           });
           setError(null);
+          console.log("Response recieved: ", message)
       } else {
           setError("No response received from the backend");
       }
@@ -40,7 +53,36 @@ const MessageProvider: React.FC<MessageProviderProps> = ({ children }) => {
         setIsLoading(false);
     }
   };
-  
+
+  // Example of new functions with hard-coded inputs
+  const testSourceCards = async () => {
+
+    const postId: bigint = BigInt(16);  // Post_id is a u64 +1 counter that starts at 1 (but resets to 1 on upgrades for now)
+    console.log("Current Post ID", postId)
+    try {
+      // const weaviateQueryResponse = await ugd_backend.get_weaviate_query("Sample Query", 1, "The_Bible"); // "The_Bible" here is the 'cluster' element of the 'author_data.ts' object.
+      // console.log("Weaviate Query Response: ", weaviateQueryResponse);
+
+      // await ugd_backend.save_sc("Example Query", "Example Author", "Example Title", "Example Heading", "Example Content");
+      // console.log("SourceCard saved successfully");
+
+      const sourceCardResponse = await ugd_backend.get_sc(postId);
+      console.log("SourceCard Response: ", sourceCardResponse);
+
+      // await ugd_backend.delete_sc(postId);
+      // console.log("SourceCard deleted successfully");
+
+      // await ugd_backend.bookmark_sc(1);
+      // console.log("SourceCard bookmarked successfully");
+
+    } catch (error) {
+      console.error("Error performing actions with hardcoded inputs: ", error);
+    }
+  };
+
+  // Calls every time for demo.
+  testSourceCards();
+
 
   return (
     <MessageContext.Provider value={{ message, updateMessage, isLoading, error, currentAuthorId, setRandomAuthorId }}>
