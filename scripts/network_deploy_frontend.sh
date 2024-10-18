540a102374de4a1c3ec024afe88ed1ec49f2a5b0
evanmcfarland <evanmcfarland.aa@gmail.com>
2024-10-18 12:36:33 -0400

    cleanup and fresh deploy

--- scripts/network_deploy_frontend.sh
+++ scripts/network_deploy_frontend.sh
@@ -9,14 +9,15 @@ cd "$SCRIPT_DIR/.." || exit 1
 # Ensure build.sh is executable
 chmod +x "$SCRIPT_DIR/build.sh"
 
+# # This step fixes azle type errors.
+# rm -rf node_modules
+# rm -rf .dfx
+# npm cache clean --force
+# npm install
+
 # Run build.sh with its full path
 "$SCRIPT_DIR/build.sh"
 
-# This step fixes azle type errors.
-rm -rf node_modules
-rm -rf .dfx
-npm cache clean --force
-npm install
 
 cp dfx_mainnet.json dfx.json
 
@@ -25,5 +26,9 @@ rm -rf local/canisters/
 cp -r ic/canisters/ local/
 cd ..
 
+# # [optional]Take a snapshot
+# dfx canister stop alex_frontend --network ic
+# dfx canister snapshot create alex_frontend --network ic
+# dfx canister start alex_frontend --network ic
 
 dfx deploy alex_frontend --network ic
