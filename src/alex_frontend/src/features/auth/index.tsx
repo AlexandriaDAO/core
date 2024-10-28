1111fda6694d02d726cdeb785daf3cfb7e763f28
Zeeshan <zeeshan.ghazi@yahoo.com>
2024-10-28 13:36:27 +0500

    fix auth button

--- src/alex_frontend/src/features/auth/index.tsx
+++ src/alex_frontend/src/features/auth/index.tsx
@@ -7,6 +7,33 @@ import { ImSpinner8 } from "react-icons/im";
 import { MdLogin, MdLogout } from "react-icons/md";
 import { getAuthClient } from "./utils/authUtils";
 
+const Processing = () => (
+	<div className="flex gap-1 items-center">
+		<span className="text-base font-normal font-roboto-condensed tracking-wider">
+			Processing
+		</span>
+		<ImSpinner8 size={18} className="animate animate-spin" />
+	</div>
+);
+
+const Logout = ({ user }: { user: string }) => (
+	<div className="flex gap-1 items-center">
+		<span className="text-base font-normal font-roboto-condensed tracking-wider">
+			{user.slice(0, 5) + "..." + user.slice(-3)}
+		</span>
+		<MdLogout size={20} />
+	</div>
+);
+
+const Login = () => (
+	<div className="flex justify-between gap-1 items-center">
+		<span className="text-base font-normal font-roboto-condensed tracking-wider">
+			Login
+		</span>
+		<MdLogin size={20} />
+	</div>
+);
+
 export default function Auth() {
 	const dispatch = useAppDispatch();
 
@@ -21,32 +48,9 @@ export default function Auth() {
 	return (
 		<div
 			onClick={handleAuthClick}
-			className="flex-shrink h-auto flex justify-center items-center gap-2.5 p-2.5 border border-solid border-white text-black
-			rounded-full cursor-pointer duration-300 transition-all"
+			className="flex-shrink h-auto flex justify-center items-center gap-2.5 p-2.5 border border-solid border-white text-[#828282] hover:text-white rounded-full cursor-pointer duration-300 transition-all"
 		>
-			{loading ? (
-				<div className="flex gap-1 items-center text-[#828282]">
-					<span className="text-base font-normal font-roboto-condensed tracking-wider">
-						Processing
-					</span>
-					<ImSpinner8 size={18} className="animate animate-spin text-black" />
-				</div>
-			) : user ? (
-				<div className="flex gap-1 items-center text-[#828282] hover:text-black">
-					<span className="text-base font-normal font-roboto-condensed tracking-wider">
-						{user.slice(0, 5) + "..." + user.slice(-3)}
-					</span>
-					<MdLogout size={20} className="text-black" />
-				</div>
-			) : (
-				<div className="flex justify-between  gap-1 items-center text-[#828282] hover:text-black">
-					<span className="text-base font-normal font-roboto-condensed tracking-wider">
-						Login
-					</span>    
-					{/* <span><FontAwesomeIcon icon={faRightToBracket} /></span> */}
-					<MdLogin size={20} className="text-black" />
-				</div>
-			)}
+			{loading ? <Processing /> : user ? <Logout user={user} /> : <Login />}
 		</div>
 	);
 }
