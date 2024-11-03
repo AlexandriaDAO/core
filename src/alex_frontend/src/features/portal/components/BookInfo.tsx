49b9b1a11763b8175990f7781f1d41c32414b5f3
Zeeshan <zeeshan.ghazi@yahoo.com>
2024-11-03 08:17:06 +0500

    replace antd modal with custom dialog, fix popup click behaviour

--- src/alex_frontend/src/features/portal/components/BookInfo.tsx
+++ src/alex_frontend/src/features/portal/components/BookInfo.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
-import { Tooltip, Modal } from "antd";
+import React from "react";
 import { Book } from "../portalSlice";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/lib/components/dialog";
+import { Info } from "lucide-react";
 
 interface IBookInfoProps {
     book?: Book;
@@ -9,47 +10,27 @@ interface IBookInfoProps {
 const BookInfo: React.FC<IBookInfoProps> = ({
     book
 }: IBookInfoProps) => {
-    const [showModal, setShowModal] = useState(false);
-
-    const handleInfoClick = (e: React.MouseEvent) => {
-        e.stopPropagation();
-        setShowModal(true);
-    };
-
     if(!book) return <></>
 
     return (
-        <>
-            <Tooltip title="Click for more info">
-                <button
-                    className="absolute top-2 right-2 bg-white text-black rounded-full w-6 h-6 flex items-center justify-center"
-                    onClick={(e) => handleInfoClick(e)}
-                >
-                    i
-                </button>
-            </Tooltip>
-            <Modal
-                title="Book Information"
-                open={showModal}
-                onCancel={() => setShowModal(false)}
-                footer={null}
-            >
-                <div>
-                    <h2>{book.title}</h2>
-                    <p>Author: {book.author_first + " " + book.author_last}</p>
-                    {/* {book.tags.map((tag) => (
-                        <p key={tag.name}>
-                            {tag.name}: {tag.value}
-                        </p>
-                    ))} */}
-
+        <div onClick={e=>e.stopPropagation()}>
+            <Dialog>
+                <DialogTrigger asChild>
+                    <div className="absolute top-2 right-2 bg-muted rounded-full">
+                        <Info size={26} className="text-primary"/>
+                    </div>
+                </DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>{book.title}</DialogTitle>
+                        <DialogDescription>Author: {book.author_first + " " + book.author_last}</DialogDescription>
+                    </DialogHeader>
                     <pre className="font-roboto-condensed font-normal text-sm overflow-auto bg-gray-600 text-white p-5">
-						<code>{JSON.stringify(book, null, 2)}</code>
-					</pre>
-                </div>
-            </Modal>
-
-        </>
+                        <code>{JSON.stringify(book, null, 2)}</code>
+                    </pre>
+                </DialogContent>
+            </Dialog>
+        </div>
     );
 }
 
