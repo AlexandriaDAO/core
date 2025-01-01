d05bf6f1a40a6b385aeb0fcb0aade8180919b0be
Zeeshan Tariq <zeeshan.ghazi@yahoo.com>
2025-01-01 11:18:01 +0500

    add build commands for ethereum and solana canisters

--- scripts/build.sh
+++ scripts/build.sh
@@ -187,6 +187,50 @@ record {
 })'
 
 
+# create and deploy sign in with ethereum provider
+dfx canister create ic_siwe_provider
+
+# opt 300000000000; /* 5 minutes */
+# opt 604800000000000; /* 1 week */
+dfx deploy ic_siwe_provider --argument $'(
+    record {
+        domain = "127.0.0.1";
+        uri = "http://127.0.0.1:4943";
+        salt = "secretsalt000";
+        chain_id = opt 1;
+        scheme = opt "http";
+        statement = opt "Login to the Alexandria";
+        sign_in_expires_in = opt 300000000000;
+        session_expires_in = opt 604800000000000;
+        targets = opt vec {
+            "'$(dfx canister id ic_siwe_provider)'";
+            "'$(dfx canister id nft_manager)'";
+            "'$(dfx canister id user)'";
+        };
+    }
+)'
+
+# create and deploy sign in with solana provider
+dfx canister create ic_siws_provider
+dfx deploy ic_siws_provider --argument $'(
+    record {
+        domain = "127.0.0.1";
+        uri = "http://127.0.0.1:4943";
+        salt = "secretsalt000";
+        chain_id = opt "mainnet";
+        scheme = opt "http";
+        statement = opt "Login to the Alexandria";
+        sign_in_expires_in = opt 300000000000;
+        session_expires_in = opt 604800000000000;
+        targets = opt vec {
+            "'$(dfx canister id ic_siws_provider)'";
+            "'$(dfx canister id nft_manager)'";
+            "'$(dfx canister id user)'";
+        };
+    }
+)'
+
+
 # echo "Backend canisters finished. Copy and paste remainder of the build script manually to deploy on the network."
 # exit 1
 
