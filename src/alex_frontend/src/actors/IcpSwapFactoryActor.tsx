fb7de293a5f30ad43846357626c3951c2ba4bfd6
Zeeshan Tariq <zeeshan.ghazi@yahoo.com>
2025-05-30 16:20:53 +0500

    legacy way of using icp_swap_factory canister/actor

--- src/alex_frontend/src/actors/IcpSwapFactoryActor.tsx
+++ src/alex_frontend/src/actors/IcpSwapFactoryActor.tsx
@@ -1,3 +1,35 @@
+// import React, { useEffect, useState } from "react";
+// import { ActorContextType } from "ic-use-actor";
+
+// import { _SERVICE } from "../../../declarations/icp_swap_factory/icp_swap_factory.did";
+
+// import { ReactNode } from "react";
+// import { useIdentity } from "@/hooks/useIdentity";
+// import { IcpSwapFactoryContext } from "@/contexts/actors";
+// import { getIcpSwapFactoryCanister } from "@/features/auth/utils/authUtils";
+
+// export default function IcpSwapFactoryActor({ children }: { children: ReactNode }) {
+//     const { identity, isInitializing, isLoggingIn } = useIdentity();
+//     const [actor, setActor] = useState<ActorContextType<_SERVICE> | null>(null);
+
+//     useEffect(() => {
+//         const fetchActor = async () => {
+//             const actor = await getIcpSwapFactoryCanister();
+//             setActor({ actor });
+//         };
+//         fetchActor();
+//     }, [identity]);
+
+//     if (isInitializing || isLoggingIn || !actor) return <>{children}</>;
+
+//     return (
+//         <IcpSwapFactoryContext.Provider value={actor}>
+//             {children}
+//         </IcpSwapFactoryContext.Provider>
+//     );
+// }
+
+
 import React from "react";
 import { ActorProvider } from "ic-use-actor";
 import { canisterId, idlFactory } from "../../../declarations/icp_swap_factory";
